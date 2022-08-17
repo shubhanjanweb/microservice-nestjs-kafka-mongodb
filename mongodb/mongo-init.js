0ed19206a920bda504675b3d98e15ed14d5e68c2
Shubhanjan Banerjee <shubhanjan2008@gmail.com>
2022-08-17 17:05:19 +0530

    aws changes

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -13,6 +13,76 @@ db = new Mongo().getDB("SkillTrackerDB");
 
 db.createCollection('skills', { capped: false });
 db.createCollection('profiles', { capped: false });
+db.profiles.insert([
+  {
+    "name": "Ajay Malakar",
+    "associateId": "CTS001",
+    "mobile": "5469865496",
+    "email": "ajay.malakar@cts.com",
+    "skills": [
+      {
+        "skillId": "SK001",
+        "expertiseLevel": 20
+      }
+    ]
+  },
+  {
+    "name": "Niloni Das",
+    "associateId": "CTS003",
+    "mobile": "5469765497",
+    "email": "niloni.das@cts.com",
+    "skills": [
+      {
+        "skillId": "SK009",
+        "expertiseLevel": 15
+      },
+      {
+        "skillId": "SK010",
+        "expertiseLevel": 20
+      }
+    ]
+  },
+  {
+    "name": "Ajay Das",
+    "associateId": "CTS002",
+    "mobile": "8569765497",
+    "email": "ajay.das@cts.com",
+    "skills": [
+      {
+        "skillId": "SK005",
+        "expertiseLevel": 9
+      },
+      {
+        "skillId": "SK006",
+        "expertiseLevel": 6
+      }
+    ]
+  },
+  {
+    "name": "Twinkle Khanna",
+    "associateId": "CTS004",
+    "mobile": "8565985497",
+    "email": "twinkle.khanna@cts.com",
+    "skills": [
+      {
+        "skillId": "SK001",
+        "expertiseLevel": 9
+      },
+      {
+        "skillId": "SK002",
+        "expertiseLevel": 6
+      },
+      {
+        "skillId": "SK003",
+        "expertiseLevel": 9
+      },
+      {
+        "skillId": "SK006",
+        "expertiseLevel": 6
+      }
+    ]
+  }
+]);
 
 db.skills.insert([
   { "skillId": "SK001", "skillName": "HTML-CSS-JAVASCRIPT", "skillType": "Technical" },
