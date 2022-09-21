793d31045967623fa321987e95c0a0f578727193
Shubhanjan Banerjee <shubhanjan2008@gmail.com>
2022-09-21 21:03:59 +0530

    Updates

--- add-profile-micro-service/src/main.ts
+++ add-profile-micro-service/src/main.ts
@@ -5,20 +5,20 @@ import { getConfig } from './app-config';
 
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: [getConfig().kafka.brokers],
-        },
-        consumer: {
-          groupId: getConfig().kafka.groupId,
-        },
+  const app = await NestFactory.create(AppModule);
+  const microservice = app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [getConfig().kafka.brokers],
+      },
+      consumer: {
+        groupId: getConfig().kafka.groupId,
       },
     },
-  );
-  await app.listen();
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(80);
 }
 bootstrap();
--- mongodb/docker.aws.push.bat
+++
@@ -1,1 +0,0 @@
-pnpm docker:aws:build
